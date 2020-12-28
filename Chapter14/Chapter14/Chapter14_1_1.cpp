65e0f458869bb91001f23d48b0c38d5613218183
jeha0714 <whrydus0714@gmail.com>
2020-12-28 19:48:25 +0900

    Chapter14_ver2

--- Chapter14/Chapter14/Chapter14_1_1.cpp
+++ Chapter14/Chapter14/Chapter14_1_1.cpp
@@ -1,8 +1,23 @@
-#include <stdio.h>
+/*#include <stdio.h>
+
+int SquareByValue(int num) {
+	return num * num;
+}
+
+void SquareByReference(int* num) {
+	*num = (*num) * (*num);
+}
 
 int main(void) {
+	int num;
 
+	scanf("%d", &num);
+
+	printf("%d \n", SquareByValue(num));
+
+	SquareByReference(&num);
+	printf("%d \n", num);
 
 
 	return 0;
-}
+}*/
